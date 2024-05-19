619fb687a40f0f050d2bee62109382ee0c3d6293
josephine19001 <gyameraj@gmail.com>
2024-05-19 09:48:04 +0300

    Set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <>hello world</>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/root';
+import Resume from './routes/resume';
+import Blogs from './routes/blogs';
+import BlogPage from './routes/blogs/blog-page';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:blogId/:title" element={<BlogPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
