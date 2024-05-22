1490612fa1af21b712beee36c1b8816752555fab
josephine19001 <gyameraj@gmail.com>
2024-05-22 21:03:17 +0300

    Resume page

--- src/routes/resume.jsx
+++ src/routes/resume.jsx
@@ -1,9 +1,18 @@
 import Layout from '../components/shared/Layout';
+import LeftSide from '../components/resume/LeftSide';
+import RightSide from '../components/resume/RightSide';
 
 const Resume = () => {
   return (
     <Layout>
-      <div>Resume page</div>
+      <div className="mt-10 flex flex-wrap md:flex-nowrap gap-5">
+        <div className="w-full md:w-3/6">
+          <LeftSide />
+        </div>
+        <div className="w-full">
+          <RightSide />
+        </div>
+      </div>
     </Layout>
   );
 };
